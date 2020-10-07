8162a9771121736b2c0a87553c81cd8bc802407a
Jonathan McGill <jonxmcgill27@gmail.com>
2020-10-07 13:04:40 -0500

    Added Car Rrpo

---
+++ CarApp.DataAccess/Data/Repositories/CarRepository.cs
@@ -0,0 +1,17 @@
+﻿using CarApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarApp.DataAccess.Data.Repositories
+{
+    public class CarRepository : Repository<Car>, ICarRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CarRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+    }
+}
