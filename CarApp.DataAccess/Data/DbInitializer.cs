8d0815a6dd9e8f02b15738efbcc2d980f5c3d1cb
Carlos Diaz <carlosfdiazramos@gmail.com>
2020-10-08 13:06:54 -0500

    update

--- CarApp.DataAccess/Data/DbInitializer.cs
+++ CarApp.DataAccess/Data/DbInitializer.cs
@@ -1,7 +1,10 @@
 ﻿using CarApp.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
 using System;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 
 namespace CarApp.DataAccess.Data
@@ -10,11 +13,13 @@ namespace CarApp.DataAccess.Data
     {
         private readonly ApplicationDbContext _db;
         private readonly UserManager<IdentityUser> _userManager;
+        private readonly IHostEnvironment _env;
 
-        public DbInitializer(ApplicationDbContext db, UserManager<IdentityUser> userManager)
+        public DbInitializer(ApplicationDbContext db, UserManager<IdentityUser> userManager, IHostEnvironment env)
         {
             _db = db;
             _userManager = userManager;
+            _env = env;
         }
 
         public async void Initialize()
@@ -43,6 +48,16 @@ namespace CarApp.DataAccess.Data
                 EmailConfirmed = true
             }, "Letmein123!").GetAwaiter().GetResult();
 
+
+            var file = _env.ContentRootFileProvider.GetFileInfo("wwwroot\\asset\\yellow-car.jpg").CreateReadStream();
+            byte[] fileBytes;
+            using (var ms = new MemoryStream())
+            {
+                file.CopyTo(ms);
+                fileBytes = ms.ToArray();
+                file.Dispose();
+            }
+
             Event event1 = new Event() {
                 Title = "Mustang Showing",
                 DateStart = new DateTime(2020, 11, 05),
@@ -51,8 +66,12 @@ namespace CarApp.DataAccess.Data
                 Description = 
                 "Come see the vintage cars. Drinks, food and gifts." +
                 "Cras justo odio, dapibus ac facilisis in, egestas eget quam. " +
-                "Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit."
+                "Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.",
+                Image = fileBytes
             };
+
+
+
 
             _db.Events.Add(event1);
            
